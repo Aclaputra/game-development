27eafe87a398c52392c7e01e35474e449eb8e347
Muhammad Acla <muhamadacla22@gmail.com>
2025-02-07 08:55:36 +0700

    add assets ninjapack

--- cmd/desktop.go
+++ cmd/desktop.go
@@ -1,141 +1,200 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"image"
 	"image/color"
+	_ "image/png"
 	"log"
 
-	"github.com/Aclaputra/game-development/config"
-	"github.com/Aclaputra/game-development/constant"
-	"github.com/Aclaputra/game-development/drawing"
-	game_map "github.com/Aclaputra/game-development/game/map"
-	"github.com/Aclaputra/game-development/game/npc"
-	"github.com/Aclaputra/game-development/model"
-
-	"github.com/hajimehoshi/ebiten/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
-	"github.com/hajimehoshi/ebiten/v2/text/v2"
-	"github.com/spf13/viper"
+)
+
+var (
+	scale         = 2.0
+	SCREEN_WIDTH  = 320
+	SCREEN_HEIGHT = 240
 )
 
 type (
 	Game struct {
-		Title string
-		Text  string
-		keys  []ebiten.Key
+		Player  *Player
+		enemies []*Enemy
+		potions []*Potion
+	}
+	Sprite struct {
+		Img  *ebiten.Image
+		X, Y float64
+	}
+	Potion struct {
+		*Sprite
+		AmtHeal uint
+	}
+	// embedded struct
+	Enemy struct {
+		*Sprite
+		FollowsPlayer bool
+	}
+	Player struct {
+		*Sprite
+		Health uint
 	}
 )
 
 func (g *Game) Update() error {
-	model.TimeCounter++
+	// player controller input
+	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyD) {
+		g.Player.X += 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyA) {
+		g.Player.X -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyW) {
+		g.Player.Y -= 2
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyS) {
+		g.Player.Y += 2
+	}
 
-	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
-	if len(g.keys) > 0 {
-		switch g.keys[0].String() {
-		case "W":
-			model.SkeletonDirection = "north"
-		case "A":
-			model.SkeletonDirection = "west"
-		case "S":
-			model.SkeletonDirection = "south"
-		case "D":
-			model.SkeletonDirection = "east"
+	for _, enemy := range g.enemies {
+		if enemy.FollowsPlayer {
+			if enemy.X < g.Player.X {
+				enemy.X += 1
+			} else if enemy.X > g.Player.X {
+				enemy.X -= 1
+			}
+
+			if enemy.Y < g.Player.Y {
+				enemy.Y += 1
+			} else if enemy.Y > g.Player.Y {
+				enemy.Y -= 1
+			}
 		}
 	}
-	skeleton := npc.NewSkeleton(model.SkeletonDirection)
-	if err := skeleton.Render(); err != nil {
-		return err
+
+	for _, potion := range g.potions {
+		if g.Player.X > potion.X {
+			g.Player.Health += potion.AmtHeal
+			fmt.Printf("Picked up potion health increased by %v, current Health %v\n", potion.AmtHeal, g.Player.Health)
+		}
 	}
 
 	return nil
 }
 
+// stacking dari atas ke bawah
 func (g *Game) Draw(screen *ebiten.Image) {
-	const (
-		normalFontSize = 24
-		bigFontSize    = 48
+	screen.Fill(color.RGBA{120, 180, 255, 255})
+
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Scale(scale, scale)
+	opts.GeoM.Translate(g.Player.X, g.Player.Y)
+
+	// draw player
+	screen.DrawImage(
+		g.Player.Img.SubImage(
+			image.Rect(0, 0, 16, 16),
+		).(*ebiten.Image),
+		&opts,
 	)
-	var (
-		x   = 20
-		msg = fmt.Sprintf("%s - TPS: %0.2f", g.Text, ebiten.ActualTPS())
-	)
 
-	drawText := drawing.NewDrawText(&text.DrawOptions{})
-	drawText.UpperHeader(screen, x, msg, model.ArcadeFontText, normalFontSize)
-	drawText.MiddleHeader(screen, 0, color.White, g.Title, model.ArcadeFontText, normalFontSize)
-	drawText.BelowHeader(screen, 0, color.White, "Main Lobby", model.ArcadeFontText, normalFontSize)
+	opts.GeoM.Reset()
 
-	gameMap := game_map.NewGameMap()
-	if err := gameMap.RenderTile(0, 0, 0); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(25, 0, 1); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(50, 0, 2); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(50, 25, 3); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(25, 25, 4); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(0, 25, 5); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(0, 50, 6); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(25, 50, 7); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	if err := gameMap.RenderTile(50, 50, 8); err != nil {
-		fmt.Printf("error rendering tile: %v", err.Error())
-	}
-	drawGameMap := drawing.NewDrawSprite(&ebiten.DrawImageOptions{})
-	drawGameMap.Position(screen, model.GameTileSprite[0], float64(model.BasePosition)-50, float64(model.BasePosition)-50)
-	drawGameMap.Position(screen, model.GameTileSprite[1], 50, 0)
-	drawGameMap.Position(screen, model.GameTileSprite[2], 50, 0)
-	drawGameMap.Position(screen, model.GameTileSprite[3], 0, 50)
-	drawGameMap.Position(screen, model.GameTileSprite[4], -50, 0)
-	drawGameMap.Position(screen, model.GameTileSprite[5], -50, 0)
-	drawGameMap.Position(screen, model.GameTileSprite[6], 0, 50)
-	drawGameMap.Position(screen, model.GameTileSprite[7], 50, 0)
-	drawGameMap.Position(screen, model.GameTileSprite[8], 50, 0)
+	for _, sprite := range g.enemies {
+		opts.GeoM.Scale(scale, scale)
+		opts.GeoM.Translate(sprite.X, sprite.Y)
 
-	drawWalkingSkeleton := drawing.NewDrawSprite(&ebiten.DrawImageOptions{})
-	drawWalkingSkeleton.Position(screen, model.SkeletonSprite, float64(model.CountMovementX), float64(model.CountMovementY))
+		screen.DrawImage(
+			sprite.Img.SubImage(
+				image.Rect(0, 0, 16, 16),
+			).(*ebiten.Image),
+			&opts,
+		)
 
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("Pixel at: %v", model.SkeletonFramePixel))
+		opts.GeoM.Reset()
+	}
+
+	for _, potion := range g.potions {
+		opts.GeoM.Scale(scale, scale)
+		opts.GeoM.Translate(potion.X, potion.Y)
+
+		screen.DrawImage(
+			potion.Img.SubImage(
+				image.Rect(0, 0, 16, 16),
+			).(*ebiten.Image),
+			&opts,
+		)
+
+		opts.GeoM.Reset()
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return constant.SCREEN_WIDTH, constant.SCREEN_HEIGHT
-}
-
-func init() {
-	arcadeText, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.ArcadeN_ttf))
-	if err != nil {
-		log.Fatal(err)
-	}
-	model.ArcadeFontText = arcadeText
-
+	// return outsideWidth, outsideHeight
+	return ebiten.WindowSize()
 }
 
 func main() {
-	config.ExecConfig()
-	ebiten.SetWindowSize(constant.SCREEN_WIDTH, constant.SCREEN_HEIGHT)
-	ebiten.SetWindowTitle(viper.GetString("game.title"))
+	ebiten.SetWindowSize(SCREEN_WIDTH*2, SCREEN_HEIGHT*2)
+	ebiten.SetWindowTitle("RPG 2D")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	gameTitle := viper.GetString("game.title")
-	if err := ebiten.RunGame(&Game{
-		Title: gameTitle,
-		Text:  fmt.Sprintf("Welcome to %s", gameTitle),
-	}); err != nil {
+	var (
+		err                              error
+		playerImg, cavemenImg, potionImg *ebiten.Image
+	)
+
+	if playerImg, _, err = ebitenutil.NewImageFromFile("assets/ninjapack/Actor/Characters/NinjaGray/SpriteSheet.png"); err != nil {
+		log.Fatal(err)
+	}
+	if cavemenImg, _, err = ebitenutil.NewImageFromFile("assets/ninjapack/Actor/Characters/Caveman2/SpriteSheet.png"); err != nil {
+		log.Fatal(err)
+	}
+	if potionImg, _, err = ebitenutil.NewImageFromFile("assets/ninjapack/Items/Potion/Heart.png"); err != nil {
+		log.Fatal(err)
+	}
+
+	game := &Game{
+		Player: &Player{
+			&Sprite{
+				Img: playerImg,
+				X:   300,
+				Y:   230,
+			},
+			1000,
+		},
+		enemies: []*Enemy{
+			{
+				&Sprite{
+					Img: cavemenImg,
+					X:   200,
+					Y:   100,
+				},
+				true,
+			},
+			{
+				&Sprite{
+					Img: cavemenImg,
+					X:   250,
+					Y:   100,
+				},
+				false,
+			},
+		},
+		potions: []*Potion{
+			{
+				&Sprite{
+					Img: potionImg,
+					X:   320,
+					Y:   120,
+				},
+				100,
+			},
+		},
+	}
+
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
